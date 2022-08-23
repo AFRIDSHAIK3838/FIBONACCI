5b0487979d6cd251567000d4e3b30ccef4ec396b
Afrid <shaikrehamaan2003@gmail.com>
2022-08-23 12:39:08 +0530

    EDITED CODE

--- FIBONACCI.java
+++ FIBONACCI.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class FIBONACCI {
     public static void main(String[] args) {
-        int A=0,B=1,n3;
+        int A=0,B=1,n3,n1,n2;
+
         int Range;
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter The Range");
         Range =sc.nextInt();
-        for(int i=2;i<Range;++i)
+        for(int j=2;j<Range;j++)
         {
             n3=n1+n2;
             System.out.print(""+n3);
